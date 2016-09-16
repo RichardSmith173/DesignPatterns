f60577127a0ee20c0abd62d602d2c8a0fcb51890
Richard <Tracy@TRACY-PRECISION>
2016-09-16 20:26:51 +0100

    Command pattern with undo operation.

---
+++ CommandPattern/CommandPattern/CalculatorCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern
+{
+    class CalculatorCommand : ICommand
+    {
+        private char _operator;
+        private int _operand;
+        private Calculator _calculator;
+
+        public CalculatorCommand(Calculator calculator, char operation, int operand)
+        {
+            this._calculator = calculator;
+            this._operator = operation;
+            this._operand = operand;
+
+        }
+
+        public void Execute()
+        {
+            _calculator.Operation(_operator, _operand);
+        }
+
+        public void RollBack()
+        {
+            _calculator.Operation(Undo(_operator), _operand);
+        }
+
+        private char Undo(char @operator)
+        {
+            switch (@operator)
+            {
+                case '+': return '-';
+                case '-': return '+';
+                case '*': return '/';
+                case '/': return '*';
+                default:
+                    throw new ArgumentException("@operator");
+            }
+        }
+    }
+}
